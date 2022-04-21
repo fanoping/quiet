6e5713b7cc64de855ba8e7b1f22c095c4a3b97ad
fanoping <fanoping@gmail.com>
2022-04-21 18:01:28 +0800

    [Core] refactor qApplication

--- quiet/main.cpp
+++ quiet/main.cpp
@@ -11,25 +11,8 @@
 
 int main(int argc, char *argv[])
 {
-//    QApplication a(argc, argv);
-
     Core core(argc, argv);
 
-    // initialize all instances (TODO - move initialization into qApplication(wrapper))
-    // mainWindow should always in the last order (wait for all instances created)
-    // make connections in mainwindow ( and all nested widgets (view))
-//    g_settingsManager = Settings::getInstance();
-
-//    g_actionManager = ActionManager::getInstance();
-//    g_directoryManager = DirectoryManager::getInstance();
-//    g_imageManager = ImageManager::getInstance();
-
-//    g_mainWindow = MainWindow::getInstance();
-
-    // debug
-
-//    g_mainWindow->show();
-//    g_mainWindow->close();
     core.show();
 
     return core.exec();
