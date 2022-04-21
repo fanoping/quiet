9093683653582995e46bb0b7f252ad0f230654fa
fanoping <fanoping@gmail.com>
2022-04-21 18:01:52 +0800

    [Core] refactor qApplication

--- quiet/main.cpp
+++ quiet/main.cpp
@@ -1,13 +1,6 @@
 #include <QApplication>
 #include "core/core.h"
 
-#include "mainwindow.h"
-#include "model/imagemanager.h"
-#include "model/actionmanager.h"
-#include "model/directorymanager.h"
-#include "settings.h"
-#include "object/node.h"
-
 
 int main(int argc, char *argv[])
 {
